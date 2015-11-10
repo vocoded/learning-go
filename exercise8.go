19e3716c662ff491e5d4492ecb878209e6ad3d60
Thomas Hutchings <vocoded@users.noreply.github.com>
2015-11-09 21:07:13 -0800

    Method refactor

--- exercise8.go
+++ exercise8.go
@@ -5,13 +5,19 @@ import (
   "github.com/vocoded/learning-go/utils"
 )
 
-// The old standby - reverse a string
-func exercise8() {
-  input := utils.GetInput("Enter some text: ")
+func Reverse(input string) string {
   length := len(input)
   reverse := make([]rune, length, length)
   for i, c := range input {
     reverse[length - i - 1] = c
   }
-  fmt.Println("The reverse is", string(reverse))
+  
+  return string(reverse)
+}
+
+// The old standby - reverse a string
+func exercise8() {
+  input := utils.GetInput("Enter some text: ")
+  reverse := Reverse(input)
+  fmt.Println("The reverse is", reverse)
 }
